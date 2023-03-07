c222e271f486f2b344d8504fbc3ba25b1a2d209d
Justin Signo <justinsigno@yahoo.com>
2023-03-07 17:08:56 -0500

    testing responsiveness

--- sketch.js
+++ sketch.js
@@ -1,14 +1,12 @@
-
-const density = '你很美丽'
+const density = "你很美丽";
 let video;
 
-
 function setup() {
-  createCanvas(720,450);
+  createCanvas(720, 450);
   video = createCapture(VIDEO);
-  video.size(72,45);
+  video.size(72, 45);
   video.hide();
-  button = createButton('save');
+  button = createButton("save");
   button.position(674, 506);
   button.mousePressed(keyPressed);
 }
@@ -21,19 +19,19 @@ function draw() {
 
   for (let j = 0; j < video.height; j++) {
     for (let i = 0; i < video.width; i++) {
-      const pixelIndex = (video.width -i + 1 + (j * video.width)) * 4;
+      const pixelIndex = (video.width - i + 1 + j * video.width) * 4;
       const r = video.pixels[pixelIndex + 0];
       const g = video.pixels[pixelIndex + 1];
       const b = video.pixels[pixelIndex + 2];
       const avg = (r + g + b) / 3;
-      
+
       noStroke();
       fill(avg);
 
       const len = density.length;
       const charIndex = floor(map(avg, 0, 255, 0, len));
-      
-      textAlign(CENTER,CENTER);
+
+      textAlign(CENTER, CENTER);
       text(density.charAt(charIndex), i * w + w * 0.5, j * h + h * 0.5);
     }
   }
@@ -41,6 +39,5 @@ function draw() {
 
 function keyPressed() {
   // this will download the frame
-    saveCanvas('你很美丽', 'jpg');
-
+  saveCanvas("你很美丽", "jpg");
 }
